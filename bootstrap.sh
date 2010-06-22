f6299b0d2bc55e83ccdfe6a41d4f6246685adb5c
Fred Oliveira <fred@webreakstuff.com>
2010-06-22 17:48:37 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# get the latest jQuery
+curl -o javascript/jquery.min.js http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+
+# get the latest 960gs 
+curl -o css/960.css http://github.com/nathansmith/960-Grid-System/raw/master/code/css/960.css
+curl -o css/reset.css http://github.com/nathansmith/960-Grid-System/raw/master/code/css/reset.css
+curl -o css/text.css http://github.com/nathansmith/960-Grid-System/raw/master/code/css/text.css
+
