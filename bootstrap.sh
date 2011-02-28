1399e41111da8626d5ae81121f763c5534ce8ae3
Doug Johnston <dbjohnston@gmail.com>
2011-02-28 14:49:58 -0800

    Use https for github URIs

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 curl -o javascript/jquery.min.js http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 
 # get the latest 960gs 
-curl -o css/960.css http://github.com/nathansmith/960-Grid-System/raw/master/code/css/960.css
-curl -o css/reset.css http://github.com/nathansmith/960-Grid-System/raw/master/code/css/reset.css
-curl -o css/text.css http://github.com/nathansmith/960-Grid-System/raw/master/code/css/text.css
+curl -o css/960.css https://github.com/nathansmith/960-Grid-System/raw/master/code/css/960.css
+curl -o css/reset.css https://github.com/nathansmith/960-Grid-System/raw/master/code/css/reset.css
+curl -o css/text.css https://github.com/nathansmith/960-Grid-System/raw/master/code/css/text.css
 
